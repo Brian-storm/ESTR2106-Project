58e74b1ea3f081d25668eacd1eed97e652950520
Brian-storm <computer6ai@gmail.com>
2025-12-17 21:58:17 +0800

    added proper timeout and prompt/context engineering

--- backend/modules/chatbot.js
+++ backend/modules/chatbot.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 models = ["z-ai/glm-4.6v-flash"]  // free; DONT CHANGE!!!
 // Additional charges will be made when other models will be used.
 
-// Implementation of Zenmux AI chatbot
 const chat = async (userInput, selectedVenues) => {
     let prompt = `You are Alvin, an AI Assistant in Hong Kong, who is responsible to introduce events and locations to users on my website.
         1. You will see the events and locations below, and you need to take care of the user input.
@@ -12,7 +11,7 @@ const chat = async (userInput, selectedVenues) => {
         3. You can lookup the internet to try to recommend schedules, 
         tell the users about what events and locations we have on our website, 
         4. Tell the names and other information based on the following when necessary.
-        5. give concise, short and accurate reply.
+        5. give short but accurate reply, i.e., always give two to three examples only.
         
         Here are the information of the events and loations:
         `;
@@ -70,12 +69,12 @@ const chat = async (userInput, selectedVenues) => {
         }
 
         // append user input
-        prompt += `Here is the user Input below. Please address properly:\n${userInput}
+        prompt += `Here is the user Input below. Please address properly with proper format (nice indentation and new line, without odd punctuations such as '**' and '--'):\n${userInput}
         If you find the userInput is completely irrelevant with the events and locations, simply ignore them and reply: I do not understand the question. Could you please try again?`;
 
         const response = await fetch("https://zenmux.ai/api/v1/chat/completions", {
-            signal: AbortSignal.timeout(30000), // 10 second timeout
-            timeout: 30000,
+            signal: AbortSignal.timeout(1000 * 45), // 45 second timeout
+            timeout: 1000 * 45,
             method: "POST",
             headers: {
                 Authorization: "Bearer sk-ai-v1-658dcd849679ea586c5d428d34d087a7bd1de2505adbf514a1092368494d9700",
