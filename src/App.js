7168988ee84340c901f333d2ce661c4854db3b29
jeremyting0727 <jeremy.tt0727@gmail.com>
2025-12-18 16:00:18 +0800

    Remove View component and related route
    Deleted the View component and its import from App.js. Also removed the '/view/:venueId' route, as the component is no longer needed.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Event from './components/Event';
 import Map from './components/Map';
 import Favorite from './components/Favorite';
 import Login from './components/Login';
-import View from './components/View';
 import AdminEvents from "./components/AdminEvents";
 import AdminUsers from "./components/AdminUsers";
 
@@ -164,9 +163,6 @@ function App() {
                     } />
                     <Route path='/map' element={
                         user ? <Map /> : <Navigate to="/login" replace />
-                    } />
-                    <Route path='/view/:venueId' element={
-                        user ? <View /> : <Navigate to="/login" replace />
                     } />
                     <Route path='/favorite' element={
                         user ? <Favorite /> : <Navigate to="/login" replace />
